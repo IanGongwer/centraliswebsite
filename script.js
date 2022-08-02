6d8668f99a83626c9418baa197219d0a388dbc56
Ian Gongwer <iangongwerpersonal@gmail.com>
2022-08-02 17:47:55 -0400

    added rank number in stats page

--- script.js
+++ script.js
@@ -15,16 +15,18 @@ function addItemToDOM(item, index) {
 
     let row = table.insertRow(index);
 
-    const icon = row.insertCell(0);
-    const user = row.insertCell(1);
-    const wins = row.insertCell(2);
-    const kills = row.insertCell(3);
-    const deaths = row.insertCell(4);
+    const rank = row.insertCell(0);
+    const icon = row.insertCell(1);
+    const user = row.insertCell(2);
+    const wins = row.insertCell(3);
+    const kills = row.insertCell(4);
+    const deaths = row.insertCell(5);
 
     const image = document.createElement("img");
     image.src = "https://minotar.net/avatar/" + item.player_name + "/35";
 
     icon.appendChild(image);
+    rank.textContent = counter + 1;
     user.textContent = item.player_name;
     wins.textContent = item.game_wins;
     kills.textContent = item.player_kills;
